b4ff2182a40997e75663eaaacc857c36bcdf754e
chocolatemeier <choallen631@gmail.com>
2024-12-23 01:17:12 +0800

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  getVersion: () => ipcRenderer.sendSync('get-version'),
-  onModeChange: (callback) => {
-    ipcRenderer.on('mode-change', (event, isFramelessMode) => callback(isFramelessMode));
-  },
-  submitUrl: (url) => ipcRenderer.send('submit-url', url),
-});
