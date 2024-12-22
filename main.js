74189671084552514a8f59c61ed9e681c63a2d45
meier <choallen631@gmail.com>
2024-12-23 00:07:33 +0800

    Statement adjustment

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let inputWindow;
 let isFramelessMode = false;
 
 // 默認 URL
-const defaultURL = 'https://streamkit.discord.com/overlay/voice/347764785766727680/347764785766727682?icon=true&online=true&logo=white&text_color=%23e8e2e2&text_size=14&text_outline_color=%23000000&text_outline_size=0&text_shadow_color=%23000000&text_shadow_size=0&bg_color=%231e2124&bg_opacity=0.95&bg_shadow_color=%23000000&bg_shadow_size=0&invite_code=&limit_speaking=false&small_avatars=false&hide_names=false&fade_chat=0&streamer_avatar_first=false';
+const defaultURL = '';
 
 function createInputWindow() {
   inputWindow = new BrowserWindow({
